8183fe1b5623f6516e1c7dd61ec6a5c194e546fb
Jonathan Chung <jono.chung@gmail.com>
2018-06-22 23:51:49 +0000

    Included box_nms

--- line_segmentation.py
+++ line_segmentation.py
@@ -1,4 +1,3 @@
-import multiprocessing
 import time
 import random
 import os
@@ -7,7 +6,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
 import mxnet as mx
-from mxnet.contrib.ndarray import MultiBoxPrior, MultiBoxTarget, MultiBoxDetection
+from mxnet.contrib.ndarray import MultiBoxPrior, MultiBoxTarget, MultiBoxDetection, box_nms
 import numpy as np
 from skimage.draw import line_aa
 from skimage import transform as skimage_tf
@@ -15,7 +14,9 @@ from skimage import transform as skimage_tf
 from mxnet import nd, autograd, gluon
 from mxnet.image import resize_short
 from mxboard import SummaryWriter
-from mxnet.gluon.model_zoo.vision import resnet18_v1
+from mxnet.gluon.model_zoo.vision import resnet34_v1
+
+import multiprocessing
 
 GPU_COUNT = 4
 ctx = [mx.gpu(i) for i in range(GPU_COUNT)]
@@ -24,40 +25,23 @@ mx.random.seed(1)
 from utils.iam_dataset import IAMDataset
 from utils.draw_box_on_image import draw_boxes_on_image
 
-batch_size = 32
 min_c = 0.01
+overlap_thres = 0.2
+topk = 20
+random_y_translation, random_x_translation = 0.05, 0.05
+learning_rate = 0.001
+epochs = 1500
+random_remove_box = 0.15
+
+batch_size = 32 * len(ctx)
 print_every_n = 5
 send_image_every_n = 20
 save_every_n = 20
 checkpoint_dir, checkpoint_name = "model_checkpoint", "ssd.params"
 
-# def make_cnn():
-#     p_dropout = 0.5
-
-#     cnn = gluon.nn.HybridSequential()
-#     cnn.add(gluon.nn.Conv2D(kernel_size=(3,3), padding=(1,1), channels=16, activation="relu"))
-#     cnn.add(gluon.nn.BatchNorm())
-
-#     cnn.add(gluon.nn.Conv2D(kernel_size=(3,3), padding=(1,1), channels=16, activation="relu"))
-#     cnn.add(gluon.nn.BatchNorm())
-
-#     cnn.add(gluon.nn.Conv2D(kernel_size=(3,3), padding=(1,1), channels=16, activation="relu"))
-#     cnn.add(gluon.nn.BatchNorm())
-
-#     cnn.add(gluon.nn.Conv2D(kernel_size=(3,3), padding=(1,1), channels=16, activation="relu"))
-#     cnn.add(gluon.nn.MaxPool2D(pool_size=(2,2), strides=(2,2)))
-#     cnn.add(gluon.nn.BatchNorm())
-
-#     cnn.add(gluon.nn.Conv2D(kernel_size=(3,3), padding=(1,1), channels=16, activation="relu"))
-#     cnn.add(gluon.nn.MaxPool2D(pool_size=(2,2), strides=(2,2)))
-#     cnn.add(gluon.nn.BatchNorm())
-
-#     cnn.hybridize()
-#     return cnn
-
 def make_cnn():    
-    pretrained = resnet18_v1(pretrained=True, ctx=ctx)
-    pretrained_2 = resnet18_v1(pretrained=True, ctx=mx.cpu(0))
+    pretrained = resnet34_v1(pretrained=True, ctx=ctx)
+    pretrained_2 = resnet34_v1(pretrained=True, ctx=mx.cpu(0))
     first_weights = pretrained_2.features[0].weight.data().mean(axis=1).expand_dims(axis=1)
     
     body = gluon.nn.HybridSequential()
@@ -144,8 +128,8 @@ def ssd_forward(x, body, downsamples, class_preds, box_preds, sizes, ratios):
 class SSD(gluon.Block):
     def __init__(self, num_classes, **kwargs):
         super(SSD, self).__init__(**kwargs)
-        self.anchor_sizes = [[.88, .961]]*5 # [[.54, .619], [.71, .74], [.75, .79], [.81, .85], [.88, .961]] #
-        self.anchor_ratios = [[1000, 750, 500], [2000, 1750, 1500], [3000, 2750, 2500], [2500, 2250, 2000], [500, 250, 1]] 
+        self.anchor_sizes = [[.75, .79], [.79, .84], [.81, .85], [.85, .89], [.88, .961]]
+        self.anchor_ratios = [[10, 8, 6], [9, 7, 5], [7, 5, 3], [6, 4, 2], [5, 3, 1]] 
         self.num_classes = num_classes
 
         with self.name_scope():
@@ -169,21 +153,32 @@ def training_targets(default_anchors, class_predicts, labels):
     box_target, box_mask, cls_target = MultiBoxTarget(default_anchors, labels, class_predicts)
     return box_target, box_mask, cls_target
 
-# def augment_transform(data, label):
-#     '''
-#     Function that randomly translates the input image by +-width_range and +-height_range.
-#     The labels (bounding boxes) are also translated by the same amount.
-#     data and label are converted into tensors by calling the "transform" function.
-#     '''
-#     random_y_translation, random_x_translation = 0.01, 0.01
-#     ty = random.uniform(-random_y_translation, random_y_translation)
-#     tx = random.uniform(-random_x_translation, random_x_translation)
-#     st = skimage_tf.SimilarityTransform(translation=(tx*data.shape[1], ty*data.shape[0]))
-#     data = skimage_tf.warp(data, st)
+def augment_transform(data, label):
+    '''
+    Function that randomly translates the input image by +-width_range and +-height_range.
+    The labels (bounding boxes) are also translated by the same amount.
+    data and label are converted into tensors by calling the "transform" function.
+    '''
+    ty = random.uniform(-random_y_translation, random_y_translation)
+    tx = random.uniform(-random_x_translation, random_x_translation)
+    st = skimage_tf.SimilarityTransform(translation=(tx*data.shape[1], ty*data.shape[0])) 
+    data = skimage_tf.warp(data, st, cval=1.0)
 
-#     label[:, 0] = label[:, 0] - tx
-#     label[:, 1] = label[:, 1] - ty
-#     return transform(data*255., label)
+    label[:, 0] = label[:, 0] - tx
+    label[:, 1] = label[:, 1] - ty
+    
+    index = np.random.uniform(0, 1.0, size=label.shape[0]) > random_remove_box
+    for i, should_output_bb in enumerate(index):
+        if should_output_bb == False:
+            (x, y, w, h) = label[i]
+            (x1, y1, x2, y2) = (x, y, x + w, y + h)
+            (x1, y1, x2, y2) = (x1 * data.shape[1], y1 * data.shape[0],
+                                x2 * data.shape[1], y2 * data.shape[0])
+            (x1, y1, x2, y2) = (int(x1), int(y1), int(x2), int(y2))
+            data[y1:y2, x1:x2] = 1.0
+    label = label[index, :]
+
+    return transform(data*255., label)
 
 def transform(image, label):
     max_label_n = 13
@@ -193,8 +188,8 @@ def transform(image, label):
     '''    
     image = np.expand_dims(image, axis=2)
     image = mx.nd.array(image)
-    image = resize_short(image, 224)
-    #resize_short(image, int(700/2))
+    # image = resize_short(image, 224)
+    image = resize_short(image, int(700/2))
     image = image.transpose([2, 0, 1])/255.
         
     label = label.astype(np.float32)
@@ -213,11 +208,9 @@ print("Number of training samples: {}".format(len(train_ds)))
 test_ds = IAMDataset("form_bb", output_data="bb", output_parse_method="line", train=False)
 print("Number of testing samples: {}".format(len(test_ds)))
 
-train_data = gluon.data.DataLoader(train_ds.transform(transform), batch_size, shuffle=True, last_batch="discard", num_workers=multiprocessing.cpu_count()-2)
+train_data = gluon.data.DataLoader(train_ds.transform(augment_transform), batch_size, shuffle=True, last_batch="discard", num_workers=multiprocessing.cpu_count()-2)
 test_data = gluon.data.DataLoader(test_ds.transform(transform), batch_size, shuffle=False, last_batch="discard", num_workers=multiprocessing.cpu_count()-2)
 
-learning_rate = 0.0001
-epochs = 750
 
 net = SSD(2)
 trainer = gluon.Trainer(net.collect_params(), 'adam', {'learning_rate': learning_rate, })
@@ -268,8 +261,8 @@ def run_epoch(e, network, dataloader, trainer, log_dir, print_name, update_cnn, 
                 step_size += x.shape[0]
             trainer.step(step_size)
 
-        for i, loss in enumerate(losses):
-            total_losses[i] += loss.mean()/len(ctx)
+        for index, loss in enumerate(losses):
+            total_losses[index] += loss.mean()/len(ctx)
             
         if update_metric:
             cls_metric.update([cls_target], [nd.transpose(class_predictions, (0, 2, 1))])
@@ -277,16 +270,14 @@ def run_epoch(e, network, dataloader, trainer, log_dir, print_name, update_cnn, 
         if i == 0 and e % send_image_every_n == 0 and e > 0:
             cls_probs = nd.SoftmaxActivation(nd.transpose(class_predictions, (0, 2, 1)), mode='channel')
             output = MultiBoxDetection(*[cls_probs, box_predictions, default_anchors], force_suppress=True, clip=False)
+            output = box_nms(output, overlap_thresh=overlap_thres, valid_thresh=min_c, topk=topk)
             output = output.asnumpy()
 
             number_of_bbs = 0
             predicted_bb = []
             for b in range(output.shape[0]):
                 predicted_bb_ = output[b, output[b, :, 0] != -1]
-                b_higher_than_min_c = predicted_bb_[:, 1] > min_c
-                
-                # filter all class predictions lower than min_c and remove the class prediction confidences
-                predicted_bb_ = predicted_bb_[b_higher_than_min_c, 2:]
+                predicted_bb_ = predicted_bb_[:, 2:]
                 number_of_bbs += predicted_bb_.shape[0]
                 predicted_bb_[:, 2] = predicted_bb_[:, 2] - predicted_bb_[:, 0]
                 predicted_bb_[:, 3] = predicted_bb_[:, 3] - predicted_bb_[:, 1]
